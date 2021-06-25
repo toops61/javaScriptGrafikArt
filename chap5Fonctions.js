b2d741f57b264ffb06b3b35c75eecaa624e1a570
Toops61 <toops61@gmail.com>
2021-06-25 17:08:53 +0200

    fin chap5 fonctions

--- chap5Fonctions.js
+++ chap5Fonctions.js
@@ -11,6 +11,8 @@ function multiplie(a, b) {
     return a * b;
 } */
 
+
+//exercice moyenne classe
 let classeCm1A = [{
     nom: 'Marc',
     moyenne: 15
@@ -62,4 +64,50 @@ function quiALaMoyenne(eleves) {
     }
 }
 
-var phrase = 'Une phrase sans ponctuation c\'est très difficile à lire car on ne s\'arrête jamais de respirer et on finit par s\'étouffer à un moment ou l\'autre et ça peut devenir très compliqué d\'ailleurs j\'ai la tête qui tourne et je vaiiiiis...'
+
+//exercice : decoupe phrase
+let phrase = 'Une phrase sans ponctuation c\'est très difficile à lire car on ne s\'arrête jamais de respirer et on finit par s\'étouffer à un moment ou l\'autre et ça peut devenir très compliqué d\'ailleurs j\'ai la tête qui tourne et je vaiiiiis ...'
+
+function compteMots(phrase){
+let mots = phrase.toLowerCase().split(' ');
+let compteur = {};
+for (let ind = 0; ind < mots.length; ind++) {
+    let mot = mots[ind];
+    if (compteur[mot] === undefined){
+        compteur[mot] = 1;
+    }else{
+        compteur[mot]++;
+    }
+}
+return compteur;
+}
+
+
+//exercice 
+let eleve1 = {
+    nom : 'John',
+    notes : [14, 15, 16, 18, 4, 12]
+}
+
+let eleve2 = {
+    nom : 'Marc',
+    notes : [5, 20, 18, 12, 7, 16]
+}
+
+function calculMoyenne(peuImporte){
+    let somme = 0;
+    for (let i = 0; i < peuImporte.length; i++){
+            somme += peuImporte[i];
+        }
+    return somme / peuImporte.length;
+}
+
+function estMeilleur(eleve1, eleve2){
+    if(calculMoyenne(eleve1) > calculMoyenne(eleve2)){
+        console.log(eleve1.nom + ' est meilleur que ' + eleve2.nom);
+    }else if(calculMoyenne(eleve1) === calculMoyenne(eleve2)){
+        console.log(eleve1.nom + ' et ' + eleve2.nom + ' sont à égalité');
+    }else{
+        console.log(eleve2.nom + ' est meilleur que ' + eleve1.nom);
+    }
+}
