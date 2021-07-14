8b3d97316713e2f767e07bcca13d02cb6c48839b
Toops61 <toops61@gmail.com>
2021-07-14 14:39:27 +0200

    fin button spoiler

--- public/js/spoiler.js
+++ public/js/spoiler.js
@@ -1,7 +1,19 @@
-let spoilerContent = document.getElementsByClassName('spoiler-content');
+/* let spoilerContent = document.getElementsByClassName('spoiler-content');
 let bouton = document.querySelectorAll('button');
 for (let i = 0; i < spoilerContent.length; i++) {
     bouton[i].addEventListener('click', function () {
-        spoilerContent[i].classList.toggle('visible');
+        this.nextElementSibling.classList.add('visible');
+        this.parentNode.removeChild(this);
     })    
+} */
+
+let elements = document.querySelectorAll('.spoiler');
+for (let i = 0; i < elements.length; i++) {
+    let texteSpoil = elements[i].textContent;
+    elements[i].innerHTML = '<button class="btn">spoiler</button><span class="spoiler-content">' + texteSpoil + '</span>';
+    let bouton = document.querySelectorAll('button');
+    bouton[i].addEventListener('click', function () {
+        this.nextElementSibling.classList.add('visible');
+        this.parentNode.removeChild(this);
+    })
 }
