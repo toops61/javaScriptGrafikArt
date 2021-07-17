456e8c2969289a707f4683e08cffd40836afc975
Toops61 <toops61@gmail.com>
2021-07-17 15:35:32 +0200

    classes fade et in

--- public/js/TPonglets.js
+++ public/js/TPonglets.js
@@ -1,17 +1,33 @@
-let onglets = document.querySelectorAll('ul li a');
-function ciblerOnglets(param) {
+let onglets = document.querySelectorAll('.tabs li a');
+/* function ciblerOnglets(param) {
     if (param.parentNode.classList.contains('active') === false){ 
         document.querySelector('li.active').classList.remove('active');
         document.querySelector('div.tabs__content.active').classList.remove('active');
         param.parentNode.classList.add('active');
         document.querySelector(param.getAttribute('href')).classList.add('active');
         }
+} */
+function ciblerOnglets(param) {
+    if (param.classList.contains('active') === false){
+        document.querySelector('li .active').classList.remove('active');
+        let paragSelected = document.querySelector('div.tabs__content.active');
+        paragSelected.classList.add('fade');
+        paragSelected.addEventListener('transitionend', function () {
+            this.classList.remove('active','fade');
+            param.parentNode.classList.add('active');
+            document.querySelector(param.getAttribute('href')).classList.add('active','fade','in');
+        })
+    } else {
+        console.log('marche pas');
+    }
 }
+
 for (let i = 0; i < onglets.length; i++) {
     onglets[i].addEventListener('click', function () {
         ciblerOnglets(this);
     })    
 }
+
 if (window.location.hash != 0) {
     let urlHash = window.location.hash;
     let target = "a[href='" + urlHash + "']";
